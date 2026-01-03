77618410e6f64d7e3113ff27b7181db1c6ae6399
MichelEloka <elokamichel@gmail.com>
2026-01-03 11:23:28 +0100

    try agro updater

--- web-frontend/src/App.tsx
+++ web-frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 export default function App() {
   const { keycloak, initialized } = useKeycloak();
 
-  if (!initialized) return <div color="green"> Deeeev Eloka mich</div>;
+  if (!initialized) return <div color="green"> Test Deeeev Eloka mich</div>;
   if (!keycloak.authenticated) return <Login />;
 // sscgggsdfdfdf
   return <Home />;
