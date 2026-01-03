30dbb33734d2ca902ed2e4a2160cae3eb0395c78
MichelEloka <elokamichel@gmail.com>
2026-01-03 23:25:59 +0100

    tr2

--- web-frontend/vite.config.ts
+++ web-frontend/vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_')
-  const base = env.VITE_BASE_PATH || '/'
-
-  return {
-    base,
-    plugins: [
-      react({
-        babel: {
-          plugins: [['babel-plugin-react-compiler']],
-        },
-      }),
-    ],
-  }
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
 })
