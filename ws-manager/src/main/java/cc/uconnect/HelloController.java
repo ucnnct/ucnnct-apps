17e5370319506ff1310050bfb268f0cd8fba2f1a
MichelEloka <elokamichel@gmail.com>
2026-01-04 14:06:33 +0100

    ci/cd: cong backend5

--- ws-manager/src/main/java/cc/uconnect/HelloController.java
+++ ws-manager/src/main/java/cc/uconnect/HelloController.java
@@ -21,6 +21,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Messageese " + helloMessage + " " + serviceName + " from " + activeProfile;
+        return "Messageefse " + helloMessage + " " + serviceName + " from " + activeProfile;
     }
 }
