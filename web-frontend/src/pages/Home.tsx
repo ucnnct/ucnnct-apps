82674e11e0edafdca765435ffa9545723523091d
MichelEloka <elokamichel@gmail.com>
2026-01-11 09:56:34 +0100

    remove dev and staging prefix

--- web-frontend/src/pages/Home.tsx
+++ web-frontend/src/pages/Home.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
       <h3>Profil</h3>
       <pre style={{ background: "#f6f6f6", padding: 12, borderRadius: 8 }}>
-        Eloka michel G
+        Eloka michel GG
       </pre>
 
       <h3>Chat (bientôt)</h3>
